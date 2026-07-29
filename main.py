c2b266f451ab72d259541d20965d642b01138d8f
Antonis Loukis <antonhs.loukhs@gmail.com>
2026-07-29 15:56:15 +0300

    Draw player ship

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 
 def main():
@@ -11,7 +12,20 @@ def main():
 
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(
+    SCREEN_WIDTH / 2,
+    SCREEN_HEIGHT / 2,
+)
+    screen.fill("black")
+
+    player.draw(screen)
+
+    pygame.display.flip()
 
     while True:
         log_state()
@@ -22,6 +36,10 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        
+
 
 
 if __name__ == "__main__":
